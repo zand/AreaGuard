b1c6e87e637d0f6ecb2dc2e55e7f715d4c6db607
Chris Butler <zand@users.sourceforge.net>
2011-01-20 19:02:37 -0500

    Added Config.setup() to Main to load the config.

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		Console con = new Console();
+		Config.setup();
 		Calendar time;
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		try {
